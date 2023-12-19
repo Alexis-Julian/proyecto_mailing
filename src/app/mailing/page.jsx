ca7f9edfb6d370cfa12f4e62ca5a3239804fba32
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2023-12-18 21:16:18 -0300

    Agregando tabla de contactos desplegable

--- src/app/mailing/page.jsx
+++ src/app/mailing/page.jsx
@@ -1,10 +1,11 @@
 "use client"
-import TinyMCE from '../../components/TinyEditor';
+import TinyMCE, { MembersTable } from '../../components/TinyEditor';
 
 const HomePage = () => {
 	return (
 		<div>
       <h1>Hermes Espera su mensaje</h1>
+      <MembersTable/>
       <TinyMCE />
     </div>
   );
